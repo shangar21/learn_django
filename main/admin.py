b64a5c900e99f323835ed4a532cb09cb6b59ccb1
shangar21 <sharanshangar@gmail.com>
2020-05-31 11:37:48 -0400

    added markdown editor to admin page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Site
+from markdownx.widgets import AdminMarkdownxWidget
+from django.db import models
 # Register your models here.
+
+class SiteAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Date", {"fields":["site_published"]}),
+        ("Title", {"fields":["site_title"]}),
+        ("content", {"fields":["site_content"]})
+    ]
+
+    formfield_overrides = {
+        models.TextField: {'widget': AdminMarkdownxWidget()}
+    }
+
+admin.site.register(Site, SiteAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Site(models.Model):
+    site_title = models.CharField(max_length=200)
+    site_content = models.TextField()
+    site_published = models.DateTimeField("Date Published", default=timezone.now())
+
+    def __str__(self):
+        return self.site_title
