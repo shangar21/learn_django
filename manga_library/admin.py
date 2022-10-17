abc3cd9e68f1f1678843c18fa35ebd22127177dd
shangar <sharanshangar@gmail.com>
2022-10-16 22:12:37 -0400

    added changes to admin

--- manga_library/admin.py
+++ manga_library/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+class AuthorAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("name", {"fields": ["name"]})
+    ]
+
+class SeriesAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("name", {"fields": ["name"]})
+    ]
+
+class MangaAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("title", {"fields": ["title"]}),
+        ("author", {"fields": ["author"]}),
+        ("series", {"fields": ["series"]})
+    ]
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Series, SeriesAdmin)
+admin.site.register(Manga, MangaAdmin)
+
--- manga_library/models.py
+++ manga_library/models.py
@@ -3,13 +3,20 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.TextField()
+    def __str__(self):
+        return self.name
+
 
 class Series(models.Model):
     name = models.TextField()
+    def __str__(self):
+        return self.name
 
 class Manga(models.Model):
     author = models.ForeignKey(Author, on_delete = models.CASCADE)
     series = models.ForeignKey(Series, on_delete = models.CASCADE)
     title = models.TextField()
     volume_number = models.IntegerField()
+    def __str__(self):
+        return self.title
 
